887fcc6fd00fb1ef2e6de92643998f7aed85c0f8
Chinna0107 <kancharlahemanth89@gmail.com>
2025-05-26 14:13:29 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr={10,20,30,40,50,60,70,80,90};
+        int s=0;
+        for(int i=0;i<arr.length;i++){
+            s=s+arr[i];
+        }
+        System.out.print(s);
+    }
+}
